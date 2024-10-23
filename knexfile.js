9054b6eb10ff483c675f0b101ade02d90ec7c880
JBM-05 <jbzmuntasser@gmail.com>
2024-10-23 18:48:36 +0200

    forth attempt

--- knexfile.js
+++ knexfile.js
@@ -2,7 +2,7 @@ module.exports = {
   development: {
     client: 'pg',
     connection: {
-      host: 'postgresql://smart_brain_db_syv4_user:pjCqEdSF9P2PhDD5qjrB6hvQYhFjkHUF@dpg-cs3vd2bv2p9s73em51h0-a.oregon-postgres.render.com/smart_brain_db_syv4',
+      host: 'dpg-cs3vd2bv2p9s73em51h0-a.oregon-postgres.render.com',
       user: 'smart_brain_db_syv4_user',
       password: 'pjCqEdSF9P2rPhDD5qjrB6hvQYhFjkHUF',
       database: 'smart_brain_db_syv4',
@@ -15,7 +15,7 @@ module.exports = {
   production: {
     client: 'pg',
     connection: {
-      connectionString: 'postgresql://smart_brain_db_syv4_user:pjCqEdSF9P2PhDD5qjrB6hvQYhFjkHUF@dpg-cs3vd2bv2p9s73em51h0-a.oregon-postgres.render.com/smart_brain_db_syv4',
+      connectionString: 'dpg-cs3vd2bv2p9s73em51h0-a.oregon-postgres.render.com',
       ssl: { rejectUnauthorized: false }  // Required for secure connection to Render
     },
     migrations: {
