82b243d5412062ca62c81c52d4699be9d59ff7a2
JBM-05 <jbzmuntasser@gmail.com>
2024-10-23 17:47:54 +0200

    second attempt

--- knexfile.js
+++ knexfile.js
@@ -5,15 +5,19 @@ module.exports = {
       host: 'dpg-cs3vd2bv2p9s73em51h0-a.render.com',
       user: 'smart_brain_db_syv4_user',
       password: 'pjCqEdSF9P2rPhDD5qjrB6hvQYhFjkHUF',
-      database: 'smart_brain_db_syv4'
+      database: 'smart_brain_db_syv4',
+      ssl: { rejectUnauthorized: false }  // SSL option for Render's PostgreSQL
     },
     migrations: {
-      directory: './migrations'  // Directory for storing migration files
+      directory: './migrations'
     },
   },
   production: {
     client: 'pg',
-    connection: 'postgresql://smart_brain_db_syv4_user:pjCqEdSF9P2PhDD5qjrB6hvQYhFjkHUF@dpg-cs3vd2bv2p9s73em51h0-a/smart_brain_db_syv4',
+    connection: {
+      connectionString: 'postgresql://smart_brain_db_syv4_user:pjCqEdSF9P2rPhDD5qjrB6hvQYhFjkHUF@dpg-cs3vd2bv2p9s73em51h0-a/smart_brain_db_syv4',
+      ssl: { rejectUnauthorized: false }  // Required for secure connection to Render
+    },
     migrations: {
       directory: './migrations'
     }
