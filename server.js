7cf7ffe96360cd8adcce7c3af2574d8f25f054ae
JBM-05 <jbzmuntasser@gmail.com>
2024-10-23 19:02:45 +0200

    sixth attempt

--- server.js
+++ server.js
@@ -4,10 +4,14 @@ const cors = require("cors");
 const bcrypt = require("bcrypt");
 const app = express();
 const knex = require("knex");
+
+// Import controllers
 const sigin = require("./controllers/singin");
 const register = require("./controllers/register");
 const profile = require("./controllers/profile");
 const image = require("./controllers/image");
+
+// Setup knex connection to PostgreSQL database hosted on Render
 const postgres = knex({
   client: "pg",
   connection: {
@@ -19,10 +23,11 @@ const postgres = knex({
   },
 });
 
+// Middleware
 app.use(bodyParser.json());
 app.use(cors());
 
-
+// Basic endpoint to test server
 app.get("/", async (req, res) => {
   try {
     const result = await postgres.raw("SELECT NOW()");
@@ -31,10 +36,14 @@ app.get("/", async (req, res) => {
     res.status(500).json({ message: "Database connection failed", error: err });
   }
 });
+
+// Routes
 app.post("/signin", sigin.handleSigin(postgres, bcrypt));
 app.post("/register", register.handleRegister(postgres, bcrypt));
 app.get("/profile/:id", profile.handleProfile(postgres));
 app.put("/image", image.handleImage(postgres));
-app.listen(3000, () => {
-  console.log("yo pal");
+
+// Server listener
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server is running on port ${process.env.PORT || 3000}`);
 });
