f4f6e04a431440766b5cdf48930b9b8d4955ee9d
JBM-05 <jbzmuntasser@gmail.com>
2024-12-29 22:43:15 +0200

    test2

--- controllers/register.js
+++ controllers/register.js
@@ -1,76 +1,38 @@
 
-// const saltRounds = 10;
-// const handleRegister=(postgres,bcrypt)=>(req, res) => {
-//     const { name, email, password } = req.body;
-//     if(!name || !email || !password){
-//         return res.status(400).json("incorrect")
-//     }
-//     const hash = bcrypt.hashSync(password, saltRounds);
-//     postgres
-//       .transaction((trx) => {
-//         trx
-//           .insert({
-//             email: email,
-//             hash: hash,
-//           })
-//           .into("login")
-//           .returning("email")
-//           .then((loginEmail) => {
-//             return postgres("users")
-//               .returning("*")
-//               .insert({
-//                 email: loginEmail[0].email,
-//                 name: name,
-//                 joined: new Date(),
-//               })
-//               .then((user) => {
-//                 res.json(user[0]);
-//               });
-//           })
-//           .then(trx.commit)
-//           .catch(trx.rollback);
-//       })
-//       .catch((err) => res.status(400).json("unable to register"));
-//   }
-//   module.exports={
-//     handleRegister:handleRegister
-//   }
 const saltRounds = 10;
-
-const handleRegister = (postgres, bcrypt) => async (req, res) => {
-  const { name, email, password } = req.body;
-
-  if (!name || !email || !password) {
-    return res.status(400).json("Incorrect form submission");
-  }
-
-  try {
+const handleRegister=(postgres,bcrypt)=>(req, res) => {
+    const { name, email, password } = req.body;
+    if(!name || !email || !password){
+        return res.status(400).json("incorrect")
+    }
     const hash = bcrypt.hashSync(password, saltRounds);
-console.log(hash)
-    await postgres.transaction(async (trx) => {
-      const loginEmail = await trx("login")
-        .insert({
-          email: email,
-          hash: hash,
-        })
-        .returning("email");
-
-      const user = await trx("users")
-        .returning("*")
-        .insert({
-          email: loginEmail[0].email,
-          name: name,
-          joined: new Date(),
-        });
-
-      res.json(user[0]);
-    });
-  } catch (err) {
-    console.error(err); // Log the error for debugging purposes
-    res.status(400).json("Unable to register");
+    console.log(hash)
+    postgres
+      .transaction((trx) => {
+        trx
+          .insert({
+            email: email,
+            hash: hash,
+          })
+          .into("login")
+          .returning("email")
+          .then((loginEmail) => {
+            return postgres("users")
+              .returning("*")
+              .insert({
+                email: loginEmail[0].email,
+                name: name,
+                joined: new Date(),
+              })
+              .then((user) => {
+                res.json(user[0]);
+              });
+          })
+          .then(trx.commit)
+          .catch(trx.rollback);
+      })
+      .catch((err) => res.status(400).json("unable to register"));
   }
-};
-
-module.exports = {
-  handleRegister,
-};
+  module.exports={
+    handleRegister:handleRegister
+  }
